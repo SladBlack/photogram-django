0b49b0c3bd0da270996b4eb5026a4af456ecac3d
prime_coder <tahtilov.alexadner@gmail.com>
2021-06-12 13:15:39 +0400

    Upload photos with bug

--- photogram/photogram/settings.py
+++ photogram/photogram/settings.py
@@ -117,3 +117,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.joinpath('media')
---
+++ photogram/posts/views.py
@@ -0,0 +1,22 @@
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
+
+from .models import Post
+from .forms import PostForm
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        for item in self.request.FILES.getlist('image'):
+            Post.objects.create(image=item, author=self.request.user)
+        return super().form_valid(form)
