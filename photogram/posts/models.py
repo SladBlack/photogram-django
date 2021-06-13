ca279250bc5a03e56a10d2500a7177d8079119d4
prime_coder <tahtilov.alexadner@gmail.com>
2021-06-13 23:49:30 +0400

    Fix: delete files from media/

--- photogram/posts/models.py
+++ photogram/posts/models.py
@@ -2,12 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class ImageQuerySet(models.QuerySet):
+
+    def delete(self, *args, **kwargs):
+        for obj in self:
+            obj.image.delete()
+        super(ImageQuerySet, self).delete(*args, **kwargs)
+
+
 class Post(models.Model):
+    objects = ImageQuerySet.as_manager()
     author = models.ForeignKey(User,
                                null=True,
                                on_delete=models.CASCADE,
                                verbose_name='Пользователь')
     image = models.ImageField(upload_to='images/')
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        super(Post, self).delete(*args, **kwargs)
 
     def __str__(self):
         return str(self.author)
