9a8ed8d6a094245dff207e8309e91a1028d1572e
prime_coder <tahtilov.alexadner@gmail.com>
2021-06-16 23:17:22 +0400

    Edit post with permissions, created view counter

--- photogram/posts/api.py
+++ photogram/posts/api.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 
 from .models import Post
 from .serializers import PostsListSerializer
@@ -18,5 +19,21 @@ class PostModelView(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-        for item in self.request.FILES.getlist('image'):
-            Post.objects.create(image=item, author=self.request.user, tag=self.request.tag)
+        # for item in self.request.FILES.getlist('image'):
+        #     Post.objects.create(image=item, author=self.request.user, tag=self.request.tag)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        current_count_view = Post.objects.get(pk=kwargs.get('pk')).view_count
+        Post.objects.filter(pk=kwargs.get('pk')).update(view_count=current_count_view + 1)
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+    def get_permissions(self):
+        if self.action == 'update':
+            permission_classes = [permissions.IsAdminUser]
+        elif self.action == 'destroy':
+            permission_classes = [IsAuthorEntry]
+        else:
+            permission_classes = [permissions.IsAuthenticated]
+        return [permission() for permission in permission_classes]
--- photogram/posts/premissions.py
+++ photogram/posts/premissions.py
@@ -4,4 +4,4 @@ from rest_framework.permissions import BasePermission
 class IsAuthorEntry(BasePermission):
     """Автор записи или админимтратор"""
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user or obj.group.founder == request.user
+        return obj.author == request.user or request.user.is_staff
